549b8343a6cec49fb27b2f1ea7794f83ce34fa11
ShanzayEman <shanzayeman0@gmail.com>
2024-08-20 07:48:19 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    document.getElementById("demo1"). innerHTML = Date();
+}
+function change(){
+    document.getElementById("demo2").innerHTML="CHANGE THIS";
+    document.getElementById("demo2").style.fontSize = "larger" ;
+}
+window.alert("Hello kasy haa ap sab bilkul bhi khushi ni hoimill ky");
+document.write(5 + 10 +"<br>" + "bolo ab kia ya kr ky dekha sakty ho");
+let x,y,z;
+x=10;
+y=20;
+z=x*y;
